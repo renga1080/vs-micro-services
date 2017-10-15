918c02db44c634a92badf0d15582bf8c2af76975
renga1080 <32118579+renga1080@users.noreply.github.com>
2017-10-15 12:14:22 -0400

    added files via upload

---
+++ src/main/java/com/ranga/vs/microservies/vsmicroservices/VsMicroServicesApplication.java
@@ -0,0 +1,12 @@
+package com.ranga.vs.microservies.vsmicroservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VsMicroServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VsMicroServicesApplication.class, args);
+	}
+}
---
+++ src/test/java/com/ranga/vs/microservies/vsmicroservices/VsMicroServicesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ranga.vs.microservies.vsmicroservices;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VsMicroServicesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
