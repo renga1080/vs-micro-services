0b5027df6b215b0d3db29203c8c50ff2671dafdb
Rengarajan <krengarajan10@gmail.com>
2017-10-20 23:37:09 -0400

    fixing config server integration

--- vs-user-service/src/main/java/com/ranga/vs/user/UserApplication.java
+++ vs-user-service/src/main/java/com/ranga/vs/user/UserApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
@@ -14,7 +14,7 @@ import static com.ranga.vs.util.VSUtil.prepareLocal;
 import static com.ranga.vs.util.VSUtil.prepareTomcat;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableEurekaClient
 @EnableFeignClients
 @ComponentScan(basePackageClasses = com.ranga.vs.VsBasePackage.class)
 @Import({FilterConfig.class})
--- vs-user-service/src/main/java/com/ranga/vs/user/controller/UserServiceController.java
+++ vs-user-service/src/main/java/com/ranga/vs/user/controller/UserServiceController.java
@@ -3,15 +3,18 @@ package com.ranga.vs.user.controller;
 import com.ranga.vs.endpoint.UserEndpoint;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Slf4j
+@RefreshScope
 public class UserServiceController implements UserEndpoint {
 
     @Value("${user.welcomeMsg}")
+    //@Value("${vs.app.name}")
     private String welcomeMsg;
 
     @Override
